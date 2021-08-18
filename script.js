f7ad7dcc7ade502e6f152dfdc4bc54bf85b8aa46
juliocesarlab <juliostdfac@gmail.com>
2021-08-18 13:49:42 -0300

    responsible + hamburguer menu

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("funciona por favor!")
+const hambButton = document.querySelector('.hamburguer-menu');
+const menu = document.querySelector('.menu-wrapper-ul')
+
+
+hambButton.addEventListener('click', () => {
+  menu.classList.toggle('show-menu')
+  menu.classList.toggle('close-menu')
+})
 
