4888d122aca0eb8bf94c3853e60227ba72742738
juliocesarlab <juliostdfac@gmail.com>
2021-09-15 12:30:51 -0300

    initial skeleton

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const hambButton = document.querySelector('.hamburguer-menu');
 const menu = document.querySelector('.menu-wrapper-ul')
 const body = document.querySelector('#body');
+const readMore = document.querySelector('.read-more-btn')
+const aboutTextMore = document.querySelector('#aboutTextMore')
+const aboutText = document.querySelector('#aboutText')
+const aboutTextIntroduction = document.querySelector('#aboutTextIntroduction')
+
+
+/*Read more - sobreNos */
+
+readMore.addEventListener('click', () => {
+  console.log(aboutTextIntroduction)
+  if (aboutTextMore.style.display === 'none') {
+    aboutTextMore.style.display = 'block'
+    readMore.innerHTML = "Ver menos"
+    aboutText.style.overflowY = 'scroll'
+    aboutTextIntroduction.style.display = 'none'
+  } else {
+    aboutTextMore.style.display = 'none'
+    readMore.innerHTML = "Ver mais"
+    aboutText.style.overflowY = 'hidden'
+    aboutTextIntroduction.style.display = 'block'
+  } 
+
+})
+
 
 /* adjust title of editais/documents */
 
